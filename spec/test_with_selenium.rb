aa2a09a568f80dc5d6f04933016cb20f3ef9dc38
Lucas Amaral <lopesdoamaral@gmail.com>
2019-11-18 16:37:31 -0300

    Interacting with DOM and doing tests

--- spec/test_with_selenium.rb
+++ spec/test_with_selenium.rb
@@ -1,12 +1,40 @@
 require 'selenium-webdriver'
 
 describe 'opening website' do
+    
+    #ignoring/skipping a test
     example 'opening google' do
         driver = Selenium::WebDriver.for :firefox
         driver.get "https://www.google.com/"
     end
 
-    example 'interacting with browser elements' do
-        
+    example 'interacting with browser elements :id, :tag_name, send_keys' do
+        driver = Selenium::WebDriver.for :firefox
+        driver.navigate.to "http://the-internet.herokuapp.com/login"
+
+        driver.find_element(:id, "username").send_keys "tomsmith"
+        driver.find_element(:id, "password").send_keys "SuperSecretPassword!"
+        driver.find_element(:tag_name, "button").click
+        #:class, :class_name, :id, :name, :link_text ...
+
+        element = driver.find_element(:id, "flash")
+        elementText = element.text
+
+        expect(elementText).to include ("You logged into a secure area!")
+    end
+
+    example 'interacting with browser elements :class' do
+        driver = Selenium::WebDriver.for :firefox
+        driver.navigate.to "http://the-internet.herokuapp.com/login"
+
+        driver.find_element(:id, "username").send_keys "tomsmith_error"
+        driver.find_element(:id, "password").send_keys "SuperSecretPassword!"
+        driver.find_element(:tag_name, "button").click
+        #:class, :class_name, :id, :name, :link_text ...
+
+        element = driver.find_element(:class, "error")
+        elementText = element.text
+
+        expect(elementText).to include ("Your username is invalid!")
     end
 end

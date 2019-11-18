1991c8cec55e3a5f8f045b82b09b3eede5175400
Lucas Amaral <lopesdoamaral@gmail.com>
2019-11-18 16:55:37 -0300

    Adding rspec hooks before and after

--- spec/test_with_selenium.rb
+++ spec/test_with_selenium.rb
@@ -1,40 +1,53 @@
 require 'selenium-webdriver'
 
 describe 'opening website' do
+
+    before(:all) do
+        puts "Runs before any test"
+    end
+
+    before(:each) do
+        @driver = Selenium::WebDriver.for :firefox
+    end
     
     #ignoring/skipping a test
     example 'opening google' do
-        driver = Selenium::WebDriver.for :firefox
-        driver.get "https://www.google.com/"
+        @driver.get "https://www.google.com/"
     end
 
     example 'interacting with browser elements :id, :tag_name, send_keys' do
-        driver = Selenium::WebDriver.for :firefox
-        driver.navigate.to "http://the-internet.herokuapp.com/login"
-
-        driver.find_element(:id, "username").send_keys "tomsmith"
-        driver.find_element(:id, "password").send_keys "SuperSecretPassword!"
-        driver.find_element(:tag_name, "button").click
+        @driver.navigate.to "http://the-internet.herokuapp.com/login"
+        @driver.find_element(:id, "username").send_keys "tomsmith"
+        @driver.find_element(:id, "password").send_keys "SuperSecretPassword!"
+        @driver.find_element(:tag_name, "button").click
         #:class, :class_name, :id, :name, :link_text ...
 
-        element = driver.find_element(:id, "flash")
+        element = @driver.find_element(:id, "flash")
         elementText = element.text
 
         expect(elementText).to include ("You logged into a secure area!")
     end
 
     example 'interacting with browser elements :class' do
-        driver = Selenium::WebDriver.for :firefox
-        driver.navigate.to "http://the-internet.herokuapp.com/login"
-
-        driver.find_element(:id, "username").send_keys "tomsmith_error"
-        driver.find_element(:id, "password").send_keys "SuperSecretPassword!"
-        driver.find_element(:tag_name, "button").click
+        @driver.navigate.to "http://the-internet.herokuapp.com/login"
+        @driver.find_element(:id, "username").send_keys "tomsmith_error"
+        @driver.find_element(:id, "password").send_keys "SuperSecretPassword!"
+        @driver.find_element(:tag_name, "button").click
         #:class, :class_name, :id, :name, :link_text ...
 
-        element = driver.find_element(:class, "error")
+        #class="flash error"
+        element = @driver.find_element(:class, "error")
         elementText = element.text
 
         expect(elementText).to include ("Your username is invalid!")
     end
+
+    after(:all) do
+        puts "\nRuns after all tests"
+    end
+    
+    after(:each) do
+        @driver.quit
+    end
+
 end

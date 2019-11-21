9e4c0aa3259a993e7135ccf32289b8dab2df5141
Lucas Amaral <lopesdoamaral@gmail.com>
2019-11-21 16:56:39 -0300

    Changing Driver Dimensions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,22 +10,21 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.before(:all) do
-        options = Selenium::WebDriver::Firefox::Options.new()
+    options = Selenium::WebDriver::Firefox::Options.new()
         #args: [
         # -headless,
         # -start-maximized,
         #]
 
-        @driver = Selenium::WebDriver.for :firefox, options: options
+    @driver = Selenium::WebDriver.for :firefox, options: options
 
-        target_size = Selenium::WebDriver::Dimension.new(800, 600)
-        @driver.manage.window.size = target_size
-        @driver.manage.timeouts.implicit_wait = 5
-        @driver.manage.timeouts.page_load = 15
-    end
+    target_size = Selenium::WebDriver::Dimension.new(1024, 768)
+    @driver.manage.window.size = target_size
+    @driver.manage.timeouts.implicit_wait = 5
+    @driver.manage.timeouts.page_load = 15
+  end
 
-    config.after(:all) do
-        @driver.quit
-    end
-
+  config.after(:all) do
+      @driver.quit
+  end
 end
